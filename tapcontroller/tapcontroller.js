25b0fac661ac58ed9d72e57ffe8aa5965f6bcb7f
jakorten <jakorten@jksoftedu.nl>
2020-04-18 22:51:16 +0200

    Fixed parenthesis.

--- tapcontroller/tapcontroller.js
+++ tapcontroller/tapcontroller.js
@@ -58,7 +58,7 @@ module.exports = function(RED) {
             var tapNum   = get_tapNum_from_topic(msg.topic)
             var tapState = get_tapState_from_topic(msg.topic)
 
-            if ((tapNum >= 0 && tapNum < maxTaps) && (tapState.toUpperCase() == "OPEN" || tapState.toUpperCase() == "CLOSE") || tapState.toUpperCase() == "OFF"))) {
+            if ((tapNum >= 0 && tapNum < maxTaps) && (tapState.toUpperCase() == "OPEN") || (tapState.toUpperCase() == "CLOSE") || (tapState.toUpperCase() == "OFF")) {
               pwm = new Pca9685Driver(options, function(err) {
                   if (err) {
                       console.error("Communication with PCA9685 failed!");
@@ -105,7 +105,7 @@ module.exports = function(RED) {
                   }
 
 
-              }
+              })
             } else {
                 node.warn(
                     'Topic should contain a valid tap number in the range 0.. ' + maxTaps + ' followed by a "/" and OPEN or CLOSE command [' +
